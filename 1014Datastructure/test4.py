982b3183b2589dfb27af50028b3be342c1556dfd
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-10-22 12:23:35 +0900

    자료구조
    자료구조 수업내용 입니다

--- 1014Datastructure/test4.py
+++
@@ -1,3 +0,0 @@
-name = input('이름을 입력하세요 : ')
-print('My name is', name)
-print("My name is " + name)
--- 1014Datastructure/test5.py
+++
@@ -1,9 +0,0 @@
-a = input('a = ')
-b = input('b = ')
-
-a = int(a)
-b = int(b)
-
-print('-' * 10)
-print(a + b)
-print('-' * 10)
